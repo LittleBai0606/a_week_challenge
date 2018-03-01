ca9ebb16e7cc03a838d5e6a05b916d6f8aa66048
BenWhite <958255724@qq.com>
2018-03-01 11:05:38 +0800

    初步写好models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name=u'分类')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name=u'标签')
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=70, verbose_name=u'标题')
+
+    body = models.TextField(verbose_name=u'正文')
+
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name=u'创建时间')
+
+    modified_time = models.DateTimeField(auto_now=True, verbose_name=u'修改时间')
+
+    excecpt = models.CharField(max_length=200, blank=True, verbose_name=u'摘要')
+
+    category = models.ForeignKey(Category, verbose_name=u'文章分类')
+
+    tag = models.ManyToManyField(Tag, verbose_name=u'文章标签')
+
+    author = models.ForeignKey(User, verbose_name=u'文章作者')
+
+    def __str__(self):
+        return self.title
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_blog',
+        'USER': 'root',
+        'PASSWORD': 'zxc958255724',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
