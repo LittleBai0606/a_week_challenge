f6f2413d22514b6ff282330e0963e1f80b8d0917
BenWhite <958255724@qq.com>
2018-03-05 16:16:36 +0800

    修改url s

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views.serve'),
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls', namespace='blog')),
     url(r'', include('comments.urls', namespace='comments'))
